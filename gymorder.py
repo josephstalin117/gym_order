87458e919c8c1c8cde5b84ad40762b2f25f1bf2b
josephlin <josephlin117@gmail.com>
2022-03-29 15:51:53 +0800

    update order time

--- gymorder.py
+++ gymorder.py
@@ -102,7 +102,7 @@ if __name__ == "__main__":
     #}
     #openId = ''
 
-    gym_time = 17
+    gym_time = 14
 
     s = requests.Session()
     header = {"User-Agent": "Mozilla/5.0 (Linux; Android 10;  AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 MQQBrowser/6.2 TBS/045136 Mobile Safari/537.36 wxwork/3.0.16 MicroMessenger/7.0.1 NetType/WIFI Language/zh",}
